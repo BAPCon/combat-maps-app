6faa59a8dc9769f745741a951d1d47eabf50695d
Benjamin Perkins <warriorben44@gmail.com>
2024-03-01 06:24:58 -0500

    w

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },};
+const nextConfig = {};
 
 export default nextConfig;
